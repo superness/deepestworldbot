ad73cdb388378cbb7655f2a42b4b34785c175e8d
superness <super.hero.excuse@gmail.com>
2023-07-14 21:35:36 -0700

    Create floatCombatText.js

---
+++ floatCombatText.js
@@ -0,0 +1,89 @@
+let floatingText = []
+
+dw.on('hit', data => {
+    for (let hit of data) {
+        // Add a floating text at the target for the amount
+        let target = dw.findEntities((entity) => entity.id === hit.target).shift()
+        let newText = { text: hit.amount, x: target.x, y: target.y, target: hit.target, life: 2.0, maxLife: 2.0 }
+
+        if(target.id == dw.c.id)
+        {
+            newText.x -= 0.2
+        }
+        else
+        {
+            newText.x += 0.2
+        }
+
+        floatingText.push(newText)
+    }
+});
+
+
+
+
+var lastTextUpdate = new Date();
+
+
+// UI 
+dw.on("drawEnd", (ctx, cx, cy) => {
+
+    let camOffsetX = Math.round(cx * 96 - Math.floor(ctx.canvas.width / 2))
+    let camOffsetY = Math.round(cy * 96 - Math.floor(ctx.canvas.height / 2))
+
+    // floating text
+    let curTextUpdate = new Date()
+    var seconds = (curTextUpdate.getTime() - lastTextUpdate.getTime()) / 1000;
+    lastTextUpdate = curTextUpdate
+
+    //console.log(`seconds between updates ${seconds}`)
+
+    for (let text of floatingText) {
+        if (text.life < 0) continue
+
+        let x = text.x * 96 - camOffsetX
+        let y = text.y * 96 - camOffsetY
+
+        ctx.lineWidth = 4
+        ctx.fillStyle = 'black'
+        ctx.strokeStyle = `rgb(255, 0, 0, 0.8)`
+
+        ctx.fillStyle = 'white'
+
+        if (text.target == dw.c.id) {
+            ctx.lineWidth = 2
+            ctx.strokeStyle = `rgb(0, 0, 0, 0.8)`
+            ctx.fillStyle = 'red'
+        }
+
+        let fontSize = 16 + 22 * easeInOutElastic(text.life / text.maxLife)
+
+        ctx.font = `bold ${fontSize}px arial`;
+        ctx.strokeText(text.text, x, y)
+        ctx.fillText(text.text, x, y)
+
+        if (text.target == dw.c.id) {
+            text.y += 0.5 * seconds
+            text.x -= 0.3 * seconds
+        }
+        else {
+            text.y -= 0.5 * seconds
+            text.x += 0.3 * seconds
+        }
+        text.life -= seconds
+    }
+
+    floatingText = floatingText.filter(t => t.life > 0)
+})
+
+function easeInOutElastic(x) {
+    const c5 = (2 * Math.PI) / 4.5;
+
+    return x === 0
+        ? 0
+        : x === 1
+            ? 1
+            : x < 0.5
+                ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
+                : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
+}
