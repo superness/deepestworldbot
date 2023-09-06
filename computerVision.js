8f8746456a82df2e0e693c09a5c168ec9ee03818
superness <super.hero.excuse@gmail.com>
2023-09-06 13:39:36 -0700

    Update computerVision.js

--- computerVision.js
+++ computerVision.js
@@ -1391,3 +1391,99 @@ function drawLineToPOI(ctx, cx, cy, target, style, from = dw.c) {
         ctx.fill()
     }
 }
+
+
+
+function clearMenuButtons() {
+    let tempButtons = window.top.document.getElementsByClassName('temp-btn')
+
+    while(tempButtons.length > 0) {
+        tempButtons[0].remove()
+    }
+
+    window.top.document.getElementsByClassName('toggle-menu')[0].classList.add('me-1')
+    window.top.document.getElementById('menuButtonsContextMenu')?.remove()
+}
+
+function addMenuButton(title, onclick, parentDiv = window.top.document.getElementById('menuButtons')){ 
+    var newi = window.top.document.createElement('i')
+    newi.class='fa-solid'
+    newi.innerText = title
+
+    newi.onclick = () => onclick(newi)
+
+    var newMenuButton = window.top.document.createElement('div')
+    newMenuButton.className = 'ui-btn px-1 me-1 temp-btn'
+
+    newMenuButton.appendChild(newi)
+
+    parentDiv.appendChild(newMenuButton)
+}
+
+
+
+
+function addMenuButtonContextMenu() {
+    let menuButtons = window.top.document.getElementById('menuButtons')
+    let menuButtonsContextMenu  = window.top.document.createElement('div')
+    
+    menuButtonsContextMenu.className="ui ui-content invisible"
+    menuButtonsContextMenu.style="position:absolute;bottom:50px;right:5px;"
+    menuButtonsContextMenu.id='menuButtonsContextMenu'
+
+    menuButtons.appendChild(menuButtonsContextMenu)
+
+}
+
+function toggleMenuButtonContextMenu() {
+    let menuButtonsContextMenu = window.top.document.getElementById('menuButtonsContextMenu')
+    if(menuButtonsContextMenu.className.includes('invisible')) {
+        menuButtonsContextMenu.classList.remove('invisible')
+    }
+    else {
+        menuButtonsContextMenu.classList.add('invisible')
+    }
+}
+
+function addMenuContextMenuButton(title, onclick) {
+    let menuButtonsContextMenu = window.top.document.getElementById('menuButtonsContextMenu')
+
+    addMenuButton(title, onclick, menuButtonsContextMenu)
+}
+
+
+
+clearMenuButtons()
+addMenuButtonContextMenu()
+addMenuButton('⚙️', e => {
+    toggleMenuButtonContextMenu()
+})
+addMenuContextMenuButton(dw.get(`${dw.c.name}_manualmove`) ? 'Manual' : 'Auto', (e) => { 
+    let manualMove = !dw.get(`${dw.c.name}_manualmove`)
+    if(manualMove)
+    {
+        e.innerText = 'Manual'
+    }
+    else
+    {
+        e.innerText = 'Auto'
+    }
+    dw.set(`${dw.c.name}_manualmove`, manualMove)
+})
+addMenuContextMenuButton(dw.get(`showComputerVision`) ? 'VFX 🐵' : 'VFX 🙈', (e) => { 
+    let showComputerVision = !dw.get(`showComputerVision`)
+    if(showComputerVision)
+    {
+        e.innerText = 'VFX 🐵'
+    }
+    else
+    {
+        e.innerText = 'VFX 🙈'
+    }
+    dw.set(`showComputerVision`, showComputerVision)
+})
+
+
+
+
+
