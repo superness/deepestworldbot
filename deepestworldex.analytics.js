923ef142f68b6746640dc16b8bbdb273fac4ce80
superness <super.hero.excuse@gmail.com>
2024-09-03 10:32:40 -0700

    Update deepestworldex.analytics.js

--- deepestworldex.analytics.js
+++ deepestworldex.analytics.js
@@ -1,3 +1,4 @@
+
 // Analytics
 class DWAnalytics {
     constructor(character, apiBaseUrl, dw) {
@@ -12,7 +13,7 @@ class DWAnalytics {
     pruneCombatLog() {
         // Trim out old combat events
         let now = new Date()
-        this.combatLog =  this.combatLog.filter(e => (now - e.when) < 10000)
+        this.combatLog = this.combatLog.filter(e => (now - e.when) < 10000)
     }
 
     initialize(dw) {
@@ -24,7 +25,7 @@ class DWAnalytics {
 
         dw.on('loot', d => {
             for (let e of d) {
-                this.onLoot(e.item.md, e.item.qual, e.item.r ?? 0, JSON.stringify(e.item.mods))
+                this.onLoot(e.item.md, e.item.qual, e.item.r ?? 0, JSON.stringify(e.item))
             }
         })
 
@@ -46,14 +47,14 @@ class DWAnalytics {
     }
 
     publishAndFlushCombatLog() {
-        for(let combatEvent of this.combatLog) {
+        for (let combatEvent of this.combatLog) {
             dwa.onCombatLogEvent(
-                combatEvent.characterId, 
-                combatEvent.monsterName, 
-                combatEvent.damage, 
-                combatEvent.description, 
-                combatEvent.isCharacterHit, 
-                combatEvent.skillUsed, 
+                combatEvent.characterId,
+                combatEvent.monsterName,
+                combatEvent.damage,
+                combatEvent.description,
+                combatEvent.isCharacterHit,
+                combatEvent.skillUsed,
                 encodeURIComponent(combatEvent.when.toISOString()),
                 combatEvent.monsterID,
                 combatEvent.characterHP,
@@ -75,7 +76,7 @@ class DWAnalytics {
         }
 
         // Log the combat event for hitting the character or the character hitting something
-        if(hit.target == this.dw.c.id || hit.actor == this.dw.c.id) {
+        if (hit.target == this.dw.c.id || hit.actor == this.dw.c.id) {
             let isCharacterHit = hit.actor == this.dw.c.id
 
             let monster = isCharacterHit ? target : actor
@@ -84,43 +85,49 @@ class DWAnalytics {
             let hitDest = isCharacterHit ? monster : this.dw.c
 
             this.combatLog.push({
-                characterId:this.getDBId(),
-                monsterName:monster.md,
-                damage:hit.amount,
-                description:`lvl ${hitSource.level} ${hitSource.md} [${hitSource.hp}/${hitSource.hpMax}] attacking lvl ${hitDest.level} ${hitDest.md} [${hitDest.hp}/${hitDest.hpMax}] for ${hit.amount} with ${hit.md ?? 'attack'}`,
-                isCharacterHit:isCharacterHit,
-                skillUsed:(hit.md ?? "attack"),
-                when:new Date(),
-                monsterID:monster.id,
-                characterHP:this.dw.c.hp,
-                characterHPMax:this.dw.c.hpMax,
-                monsterHP:monster.hp,
-                monsterHPMax:monster.hpMax,
-                monsterLevel:monster.level,
-                monsterRarity:monster.r ?? 0
+                characterId: this.getDBId(),
+                monsterName: monster?.md ?? "NULL",
+                damage: hit.amount,
+                description: `lvl ${hitSource.level} ${hitSource.md} [${hitSource.hp}/${hitSource.maxHp}] attacking lvl ${hitDest.level} ${hitDest.md} [${hitDest.hp}/${hitDest.maxHp}] for ${hit.amount} with ${hit.md ?? 'attack'}`,
+                isCharacterHit: isCharacterHit,
+                skillUsed: (hit.md ?? "attack"),
+                when: new Date(),
+                monsterID: monster?.id ?? -1,
+                characterHP: this.dw.c.hp,
+                characterHPMax: this.dw.c.maxHp,
+                monsterHP: monster?.hp ?? -1,
+                monsterHPMax: monster?.maxHp ?? -1,
+                monsterLevel: monster?.level ?? -1,
+                monsterRarity: monster?.r ?? 0
             })
         }
 
-        if(hit.rip) {
+        if (hit.rip) {
             this.publishAndFlushCombatLog()
         }
 
+        console.log(hit)
         if (hit.rip && hit.target == dw.c.id) {
-            let deathDescription = 'dead'
+            let monsterBattleScore = Math.trunc(ComputerVision.getMonsterBattleScore(target, dw.c, true))
+            let myBattleScore = Math.trunc(ComputerVision.getMyBattleScore(dw.c, true))
+
+            let deathDescription = `${myBattleScore} vs ${monsterBattleScore}`
 
             let monstersTargettingMe = dw.findEntities(e => e.targetId && e.targetId == this.dw.c.id)
 
-            let monstersTargettingMeDescInfo = monstersTargettingMe.map(e => ({name: e.name ?? e.md, level: e.level, r: e.r, hp: e.hp }))
-            if(monstersTargettingMeDescInfo.length > 0) {
+            let monstersTargettingMeDescInfo = monstersTargettingMe
+            if (monstersTargettingMeDescInfo.length > 0) {
                 deathDescription += ` - ${JSON.stringify(monstersTargettingMeDescInfo)}`
             }
 
-            dwa.onDeath(actor.md, actor.level, actor.hpMax, deathDescription)
-            moveToSpot = dw.c.spawn;
-            this.dw.setTarget(null)
+            dwa.onDeath(actor.md, actor.level, actor.maxHp, deathDescription)
+            this.dw.setTarget(0)
 
         } else if (hit.rip && hit.actor == this.dw.c.id) {
-            dwa.onKill(target.md, target.level ?? 0, target.r ?? 0, 'kill')
+            let myBattleScore = Math.trunc(ComputerVision.getMyBattleScore(dw.c, true))
+
+            let monsterBattleScore = Math.trunc(ComputerVision.getMonsterBattleScore(target, dw.c, true))
+            dwa.onKill(target.md, target.level ?? 0, target.r ?? 0, `${myBattleScore} vs ${monsterBattleScore}`)
         }
     }
 
@@ -134,7 +141,7 @@ class DWAnalytics {
     }
 
     setDBId(id) {
-        this.dw.set(this.getDBIdKey(), id)
+        dw.set(this.getDBIdKey(), id)
     }
 
     async onStart() {
@@ -158,19 +165,18 @@ class DWAnalytics {
 
         dw.log(`you can watch your character here https://deepestworldex.com/Character?id=${this.getDBId()}`)
         console.log('you can watch your character here', `https://deepestworldex.com/Character?id=${this.getDBId()}`)
-
     }
 
     async onCombatLogEvent(characterId, monsterName, damage, description, isCharacterHit, skillUsed, when, monsterID, characterHP, characterHPMax, monsterHP, monsterHPMax, monsterLevel, monsterRarity) {
         console.log("💥 Combat Log Event");
-        const url = `${this.apiBaseUrl}/CombatLog?characterId=${characterId}&monsterName=${monsterName}&damage=${damage}&description=${description}&isCharacterHit=${isCharacterHit}&skillUsed=${skillUsed}&when=${when}&monsterID=${monsterID}&characterHP=${characterHP}&characterHPMax=${characterHPMax}&monsterHP=${monsterHP}&monsterHPMax=${monsterHPMax}&monsterLevel=${monsterLevel}&monsterRarity=${monsterRarity}`;
+        const url = `${this.apiBaseUrl}/CombatLog?characterId=${characterId}&monsterName=${monsterName}&damage=${damage}&description=${description}&isCharacterHit=${isCharacterHit}&skillUsed=${skillUsed}&when=${when}&monsterID=${monsterID}&characterHP=${characterHP}&characterHPMax=${characterHPMax}&monsterHP=${monsterHP}&monsterHPMax=${monsterHPMax}&monsterLevel=${monsterLevel ?? 0}&monsterRarity=${monsterRarity}`;
         const data = await this.postJson(url);
         console.log("📜 Logged combat event.");
         return data;
     }
 
     async onDeath(nameOfMurderer, levelOfMurderer, maxHPOfMurderer, description = "death", when = encodeURIComponent(new Date().toISOString())) {
-        console.log("💔  The weight of loss is heavy. Yet we endure, carrying their memory within us.");
+        logMessages.addMessage("💔  The weight of loss is heavy. Yet we endure, carrying their memory within us.");
         const url = `${this.apiBaseUrl}/CharacterDeaths/CharacterDeathsEx?characterId=${this.getDBId()}&murderer=${nameOfMurderer}&level=${levelOfMurderer}&maxHP=${maxHPOfMurderer}&description=${description}&when=${when}`;
         const data = await this.postJson(url);
         console.log("⌛  The Sands of Time hold our sorrows. Our fallen friend, remembered.");
@@ -186,7 +192,7 @@ class DWAnalytics {
     }
 
     async onKill(monsterName, monsterLevel, monsterRank, description = "kill", when = encodeURIComponent(new Date().toISOString())) {
-        console.log("⚔️  We meet adversity with courage. Each victory, a testament to our resilience.");
+        logMessages.addMessage("⚔️  We meet adversity with courage. Each victory, a testament to our resilience.");
         const url = `${this.apiBaseUrl}/MonsterKills/MonsterKillsEx?characterId=${this.getDBId()}&monsterName=${monsterName}&monsterLevel=${monsterLevel}&monsterRank=${monsterRank}&description=${description}&when=${when}`;
         const data = await this.postJson(url);
         console.log("🏆  We carve our path through the echoes of the past. Each challenge, a stepping stone.");
@@ -200,7 +206,7 @@ class DWAnalytics {
     }
 
     async divineIntervention(method, url, data = null) {
-        console.log('divine inter postal', method, url, data)
+        //console.log('divine inter postal', method, url, data)
         const response = await fetch(url, {
             method: method,
             headers: {
